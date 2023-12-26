ebd36f7f014b614dbfada78b223c414c751e773a
cpcx04 <pulidocabellochristian@gmail.com>
2023-12-26 15:17:51 +0100

    Project addded

---
+++ SataApp/src/main/java/triana/salesianos/edu/SataApp/SataAppApplication.java
@@ -0,0 +1,13 @@
+package triana.salesianos.edu.SataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SataAppApplication.class, args);
+	}
+
+}
---
+++ SataApp/src/test/java/triana/salesianos/edu/SataApp/SataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package triana.salesianos.edu.SataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
